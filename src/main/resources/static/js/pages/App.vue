844aea109840575196dd7146c34bbb50c401e0ec
rerimod <rerimod@gmail.com>
2019-05-13 16:45:48 +0300

    Changed base front

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title>KMP</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <span v-if="profile">{{profile.name}}</span>
+            <v-btn v-if="profile" icon href="/logout">
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <v-container v-if="!profile">
+                Необходимо авторизоваться через
+                <a href="/login">Google</a>
+            </v-container>
+            <v-container v-if="profile">
+                <messages-list :messages="messages" />
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import MessagesList from 'components/messages/MessageList.vue'
+
+    export default {
+        components: {
+            MessagesList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
